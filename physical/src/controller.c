6c076197372ccd40d3dc5b350e2d8fb171113c9d
Dylan Turner <dylantdmt@gmail.com>
2021-03-23 16:51:44 -0400

    Implemented button pressed function

--- physical/src/controller.c
+++ physical/src/controller.c
@@ -2,9 +2,19 @@
 #include "controller.h"
 
 void controller__init(void) {
-    
+    // Initialize buttons
+    for(int i = 0; i < 9; i++) {
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
+    }
 }
 
 char controller__button_pressed(void) {
-    return 0;
+    for(int i = 0; i < 9; i++) {        // Go through all the buttons in order
+        char btn_state = gpio_get(i);
+        if(btn_state) {
+            return i;                   // Return the first one pressed
+        }
+    }
+    return 0;                           // Otherwise return no btns pressed
 }
