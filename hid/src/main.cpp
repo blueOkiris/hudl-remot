13aee8f66f127709ceb6008e5bac607d66f83d0c
Dylan Turner <dylantdmt@gmail.com>
2021-03-27 19:24:50 -0400

    Bluetooth receiving working properly

--- hid/src/main.cpp
+++ hid/src/main.cpp
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pico/stdlib.h>
-#include <bsp/board.h>
+//#include <bsp/board.h>
 #include <ButtonIds.hpp>
 #include <Wireless.hpp>
-#include <Hid.hpp>
+//#include <Hid.hpp>
 
 using namespace hudlremot;
+
+void testReceiveBluetooth();
 
 int main() {
     // Remove when using usb
     stdio_init_all();
     sleep_ms(1000);
-    printf("Hello, world!\n");
+    printf("Hudl remote v2.0 HID side\r\n");
     
-    const Hc05 bluetooth;
+    testReceiveBluetooth();
+    
+    /*const Hc05 bluetooth;
     UsbHid usb;
     
     char cmd, btnId, curr;
@@ -26,8 +30,6 @@ int main() {
         usb.update();                               // Go through update process
         
         while(bluetooth.canRead()) {
-            printf("%c", bluetooth.getDatum());
-            
             // Get the command character
             cmd = bluetooth.getDatum();
             switch(cmd) {
@@ -128,7 +130,17 @@ int main() {
         }
         
         // Do something like usb.mouseMove(stickX, stickY)
-    }
+    }*/
     
     return 0;
+}
+
+void testReceiveBluetooth() {
+    const Hc05 bluetooth;
+    
+    while(1) {
+        while(bluetooth.canRead()) {
+            printf("%c", bluetooth.getDatum());
+        }
+    }
 }
